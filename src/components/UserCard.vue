2c4482fe76a4845b9a27ecb9501b521fc04f3c41
Maksim Mikhalevich <NakkuTricks@gmail.com>
2021-04-26 19:04:02 +0700

    Компонент карточки пользователя

---
+++ src/components/UserCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <article class="user-card">
+    <div class="user-card__avatar">
+      <img src="#" alt="Аватар пользователя" />
+    </div>
+    <user-repos-list></user-repos-list>
+  </article>
+</template>
+
+<script>
+import UserReposList from "./UserReposList";
+export default {
+  name: "UserCard",
+  components: {
+    UserReposList,
+  },
+};
+</script>
+
+<style lang="scss">
+.user-card {
+  min-width: 400px;
+
+  display: flex;
+  flex-direction: column;
+
+  &__avatar {
+  }
+}
+</style>
