86e5c3959d0118c9ec2ec995095cd059ba237469
Maksim Mikhalevich <NakkuTricks@gmail.com>
2021-04-26 19:02:15 +0700

    Базовые компоненты Search и Title

---
+++ src/components/Base/BaseSearch.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="base-search">
+    <input type="text" class="base-search__input" v-model.trim="getValue" />
+    <button class="base-search__button">Найти</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseSearch",
+  props: {
+    value: {
+      type: [String, Number],
+      default: "",
+    },
+  },
+  computed: {
+    getValue: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit("setValue", value);
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.base-search {
+  display: flex;
+  &__input {
+    margin-right: 10px;
+  }
+
+  &__button {
+  }
+}
+</style>
---
+++ src/components/Base/BaseTitle.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1 class="base-title">
+    <slot></slot>
+  </h1>
+</template>
+
+<script>
+export default {
+  name: "BaseTitle",
+};
+</script>
+
+<style lang="scss">
+.base-title {
+  font-size: 40px;
+  margin-bottom: 30px;
+}
+</style>
