f3bf67e8176ef20371bfd49aa3b86e868bd8db02
Maksim Mikhalevich <NakkuTricks@gmail.com>
2021-04-26 18:58:58 +0700

    Создал роуты

---
+++ src/components/TheMain.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="the-main">
+    <router-view></router-view>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "TheMain",
+};
+</script>
+
+<style lang="scss">
+.the-main {
+  max-width: 1250px;
+  width: 100%;
+  height: 80%;
+  overflow-y: scroll;
+  scrollbar-width: none;
+
+  &::-webkit-scrollbar {
+    width: 0;
+    height: 0;
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,23 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
+import ViewHome from "../views/ViewHome";
+import ViewUsers from "../views/ViewUsers";
+
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: ViewHome,
+  },
+  {
+    path: "/users",
+    name: "users",
+    component: ViewUsers,
+  },
+];
 
 const router = new VueRouter({
   mode: "history",
