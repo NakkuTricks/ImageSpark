b07381d72f107c3ec4b5ac579949755b1c0dd3ca
Maksim Mikhalevich <NakkuTricks@gmail.com>
2021-04-26 19:00:18 +0700

    Компонент HeadHunterLink

---
+++ src/components/HeadHunterLink.vue
@@ -0,0 +1,30 @@
+<template>
+  <a
+    href="https://spb.hh.ru/resume/f5daa269ff0854d2570039ed1f677a417a4757"
+    target="_blank"
+    class="head-hunter-link"
+  >
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/7/79/HeadHunter_logo.png"
+      alt="HeadHunter logo"
+      class="head-hunter-link__img"
+    />
+  </a>
+</template>
+
+<script>
+export default {
+  name: "HeadHunter",
+};
+</script>
+
+<style lang="scss">
+.head-hunter-link {
+  height: 70px;
+  width: 70px;
+  &__img {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
