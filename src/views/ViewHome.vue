88c1af94c44b42fcaee312c406034b5869e7ed5c
Maksim Mikhalevich <NakkuTricks@gmail.com>
2021-04-26 19:03:00 +0700

    Страница Home

---
+++ src/views/ViewHome.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="view-home">
+    <base-title class="view-home__title visually-hidden">
+      <template v-slot>Домашняя страница:</template>
+    </base-title>
+    <router-link class="view-home__link" :to="{ name: 'users' }"
+      >Перейти на страницу пользователей</router-link
+    >
+  </section>
+</template>
+
+<script>
+import BaseTitle from "../components/Base/BaseTitle";
+export default {
+  name: "ViewHome",
+  components: {
+    BaseTitle,
+  },
+};
+</script>
+
+<style lang="scss"></style>
