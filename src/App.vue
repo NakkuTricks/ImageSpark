5a5606ecad5b078d28b4040a2081e9c5f7e77859
Maksim Mikhalevich <NakkuTricks@gmail.com>
2021-04-26 18:59:58 +0700

    Компонент App

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,32 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <the-header></the-header>
+    <the-main></the-main>
+  </div>
 </template>
 
-<style lang="scss"></style>
+<script>
+import TheHeader from "./components/TheHeader";
+import TheMain from "./components/TheMain";
+export default {
+  name: "App",
+  components: {
+    TheHeader,
+    TheMain,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+
+  width: 100%;
+  height: 100vh;
+
+  padding: 0 35px;
+}
+</style>
