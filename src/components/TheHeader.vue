d5c865a0d8483b45cabed334ec8934f313208c34
Maksim Mikhalevich <NakkuTricks@gmail.com>
2021-04-26 19:00:48 +0700

    Компонент TheHeader

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="the-header">
+    <main-logo></main-logo>
+    <head-hunter-link></head-hunter-link>
+  </header>
+</template>
+
+<script>
+import MainLogo from "./MainLogo";
+import HeadHunterLink from "./HeadHunterLink";
+export default {
+  name: "TheHeader",
+  components: {
+    MainLogo,
+    HeadHunterLink,
+  },
+};
+</script>
+
+<style lang="scss">
+.the-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1400px;
+  width: 100%;
+  height: 10%;
+}
+</style>
