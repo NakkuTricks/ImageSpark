97dcc3fb347c9aa582aae9832e47ba7f04ad5482
Maksim Mikhalevich <NakkuTricks@gmail.com>
2021-04-26 19:02:50 +0700

    Страница Users

---
+++ src/views/ViewUsers.vue
@@ -0,0 +1,57 @@
+<template>
+  <section class="view-users">
+    <base-title>
+      <template v-slot>Поиск пользователей:</template>
+    </base-title>
+    <base-search
+      class="view-users__input"
+      :value="users"
+      @setValue="setUsers"
+    ></base-search>
+    <div class="view-users__wrapper">
+      <user-layout></user-layout>
+      <user-card></user-card>
+    </div>
+  </section>
+</template>
+
+<script>
+import BaseSearch from "../components/Base/BaseSearch";
+import BaseTitle from "../components/Base/BaseTitle";
+import UserLayout from "../components/UserLayout";
+import UserCard from "../components/UserCard";
+export default {
+  name: "ViewUsers",
+  components: {
+    BaseSearch,
+    BaseTitle,
+    UserCard,
+    UserLayout,
+  },
+  data() {
+    return {
+      users: "",
+    };
+  },
+  methods: {
+    setUsers(value) {
+      this.users = value;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.view-users {
+  height: 100%;
+
+  &__wrapper {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  &__input {
+    margin-bottom: 60px;
+  }
+}
+</style>
