ffd3921396c467c34d10a4c373cdcf1c9c71c52f
Maksim Mikhalevich <NakkuTricks@gmail.com>
2021-04-26 19:04:33 +0700

    Компонент списка репозиториев

---
+++ src/components/UserReposList.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul class="user-repos-list">
+    <user-repos-list-item></user-repos-list-item>
+  </ul>
+</template>
+
+<script>
+import UserReposListItem from "./UserReposListItem";
+export default {
+  name: "UserReposList",
+  components: {
+    UserReposListItem,
+  },
+};
+</script>
+
+<style lang="scss"></style>
---
+++ src/components/UserReposListItem.vue
@@ -0,0 +1,11 @@
+<template>
+  <li class="user-repos-list-item">repos</li>
+</template>
+
+<script>
+export default {
+  name: "UserReposListItem",
+};
+</script>
+
+<style lang="scss"></style>
