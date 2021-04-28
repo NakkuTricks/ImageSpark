fbd4a4a4cf9bbdb2fb992043b54bc76da8002ae4
Maksim Mikhalevich <NakkuTricks@gmail.com>
2021-04-28 12:46:05 +0700

    Реализовал добавление в localStorage

--- src/store/modules/Users.js
+++ src/store/modules/Users.js
@@ -2,19 +2,27 @@ import { API_URL } from "@/constants";
 
 export default {
   state: {
-    users: [],
-    user: {},
-    amount: 0,
+    users: JSON.parse(localStorage.getItem("users") || "[]"),
+    user: JSON.parse(localStorage.getItem("user") || "{}"),
+    amount: JSON.parse(localStorage.getItem("users-amount") || "0"),
   },
   actions: {
     getUserList({ commit }, userName) {
-      fetch(`${API_URL}/search/users?q=${userName}`)
+      fetch(`${API_URL}/search/users?q=${userName}`, {
+        headers: {
+          Accept: "application/vnd.github.v3+json",
+        },
+      })
         .then((users) => users.json())
         .then((users) => commit("setUsersToState", users))
         .catch((error) => console.log(error));
     },
     getUserCard({ commit }, userLogin) {
-      fetch(`${API_URL}/users/${userLogin}`)
+      fetch(`${API_URL}/users/${userLogin}`, {
+        headers: {
+          Accept: "application/vnd.github.v3+json",
+        },
+      })
         .then((user) => user.json())
         .then((user) => commit("setUserCardToState", user))
         .catch((error) => console.log(error));
@@ -25,9 +33,13 @@ export default {
       const user = users.items;
       state.users = user;
       state.amount = Number(users.total_count);
+
+      localStorage.setItem("users", JSON.stringify(state.users));
+      localStorage.setItem("users-amount", JSON.stringify(state.amount));
     },
     setUserCardToState(state, user) {
       state.user = user;
+      localStorage.setItem("user", JSON.stringify(state.user));
     },
   },
   getters: {},
