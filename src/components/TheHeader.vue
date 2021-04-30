c462bf70d53977efd5b3d1d22d3ccf80ee34d058
Maksim Mikhalevich <NakkuTricks@gmail.com>
2021-04-30 12:40:13 +0700

    Добавил background

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -25,5 +25,10 @@ export default {
   max-width: 1400px;
   width: 100%;
   height: 10%;
+
+  padding: 0 20px;
+
+  background: linear-gradient(90deg, #f5d7ff 0, #c9f5ff 52.75%, #6bcdff 102.3%);
+  border-radius: 30px;
 }
 </style>
