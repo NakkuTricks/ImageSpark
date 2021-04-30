d30943dae2f44c1d8fab62c2de5301156bc0318b
Maksim Mikhalevich <NakkuTricks@gmail.com>
2021-04-30 12:43:37 +0700

    Изменил стили для BaseTitle

--- src/components/Base/BaseTitle.vue
+++ src/components/Base/BaseTitle.vue
@@ -14,5 +14,10 @@ export default {
 .base-title {
   font-size: 40px;
   margin-bottom: 30px;
+
+  text-transform: uppercase;
+  -webkit-text-fill-color: transparent;
+  -webkit-text-stroke-width: 1px;
+  -webkit-text-stroke-color: #000;
 }
 </style>
